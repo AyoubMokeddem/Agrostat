5efe6d6befe60ab936ea7e21048981e9e545f650
Ayoub Mokeddem <raouf.benhaya@gmail.com>
2019-06-09 21:40:15 +0100

    Styling Courses_preview activity, adding CardViews and ScrollView

--- app/src/main/java/ayoubmokeddem/github/com/agro_stat/Courses_preview.java
+++ app/src/main/java/ayoubmokeddem/github/com/agro_stat/Courses_preview.java
@@ -3,11 +3,13 @@ package ayoubmokeddem.github.com.agro_stat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static ayoubmokeddem.github.com.agro_stat.R.layout.activity_courses_preview;
+
 public class Courses_preview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_courses_preview);
+        setContentView(activity_courses_preview);
     }
 }
